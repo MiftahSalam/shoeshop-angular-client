273dfd4e02a26697324d20dda7c2eff4e761a5d3
Miftah Salam <salam.miftah@gmail.com>
2023-04-07 09:46:08 +0700

    support electron

---
+++ shoeshop/main.js
@@ -0,0 +1,19 @@
+const { app, BrowserWindow } = require("electron");
+const url = require("url");
+const path = require("path");
+
+function onReady() {
+  win = new BrowserWindow({
+    width: 1000,
+    height: 900,
+  });
+  win.loadURL(
+    url.format({
+      pathname: path.join(__dirname, "dist/shoeshop/index.html"),
+      protocol: "file:",
+      slashes: true,
+    })
+  );
+}
+
+app.on("ready", onReady);
--- shoeshop/src/environments/environment.ts
+++ shoeshop/src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  graphqlApi: 'http://prod.graph.co.id/graph',
+  // graphqlApi: 'http://prod.graph.co.id/graph',
+  graphqlApi: 'http://localhost:8001/graphql',
 };
